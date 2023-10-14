de73d43ee33b842a4a26f24c0acfd9a72d4ff133
Alfredo Aranda <fredoaranda@gmail.com>
2023-10-13 18:55:55 -0600

    final cors update

--- logout.php
+++ logout.php
@@ -1,11 +1,12 @@
 <?php
 
 
-session_start();
+
 
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: https://php-react-movie-app-production.up.railway.app");
+
 
 
 
